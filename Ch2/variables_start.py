0e0bad099684815777e7e1bd2068c72014dac462
hoangphung21 <phunghoang211195@gmail.com>
2021-06-01 21:07:09 -0400

    variables and expressions

--- Ch2/variables_start.py
+++ Ch2/variables_start.py
@@ -1,19 +1,30 @@
-# 
+
 # Example file for variables
-#
+
 
 # Declare a variable and initialize it
-
-
+f = 0
+# print(f)
 
 # re-declaring the variable works
-
-
+# f = "abc"
+# print(f)
 
 # ERROR: variables of different types cannot be combined
-
-
+# print("This is a string"+str(123))
 
 # Global vs. local variables in functions
 
 
+def someFunc():
+    global f
+    f = "def"
+    print(f)
+
+
+someFunc()
+print(f)
+print(type(f))
+
+del f
+print(f)
