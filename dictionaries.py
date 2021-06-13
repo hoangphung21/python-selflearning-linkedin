3a8d04ebc4bc84ac28c6fc4b724c5850abf5b50e
hoangphung21 <phunghoang211195@gmail.com>
2021-06-13 15:33:56 -0400

    working with dicitonaries

--- dictionaries.py
+++ dictionaries.py
@@ -56,13 +56,103 @@
 # print(point_value)
 
 # Excercies 1
-customer1 = {
-    'first_name': 'Hoang',
-    'last_name': 'Phung',
-    'age': 26,
-    'city': 'Toronto'
-}
-print(customer1)
+# customer1 = {
+#     'first_name': 'Hoang',
+#     'last_name': 'Phung',
+#     'age': 26,
+#     'city': 'Toronto'
+# }
+# print(customer1)
 
-for keys, values in customer1.items():
-    print(keys, ":", values)
+# for keys, values in customer1.items():
+#     print(keys, ":", values)
+
+# looping through a dictionay
+# favourite_laguages = {
+#     'jen': 'python',
+#     'sarah': 'c',
+#     'edward': 'ruby',
+#     'phil': 'python'
+# }
+# for name, language in favourite_laguages.items():
+#     print(f"{name.title()}'s favorite language is: {language.title()}")
+# # looping through values, using set to avoid duplicated values
+# print("The languages have been mentioned are:")
+# for language in set(favourite_laguages.values()):
+#     print(language.title())
+# # list of friends
+# friends = ['phil', 'sarah', 'david', 'edward', 'jen', 'ani', 'hoang']
+# for name in favourite_laguages.keys():
+#     print(name.title())
+#     if name in friends:
+#         language = favourite_laguages[name].title()
+#         print(f"\t{name.title()}, I see you love {language}!")
+# for friend in friends:
+#     if friend in sorted(favourite_laguages.keys()):
+#         print(f"{friend.title()}, thank you for taking the poll.")
+#     else:
+#         print(f"{friend.title()} please take the poll!")
+
+# NESTING
+# # a List of dictionaries
+# import random
+
+# colors = ['red', 'green', 'yellow', 'black', 'white', 'blue', 'pink']
+# speed_types = ['slow', 'medium', 'fast']
+
+
+# aliens = []
+
+# for alien_number in range(30):
+#     new_aliens = {'color': random.choice(colors),
+#                   'points': random.randint(1, 10),
+#                   'speed': random.choice(speed_types)}
+#     aliens.append(new_aliens)
+# # show the first 5 aliens
+# for alien in aliens[:5]:
+#     print(alien)
+# print(f"Total number of aliens: {len(aliens)}")
+
+# # A list in a Dictionary
+# Store information about a pizza being ordered
+# pizza = {
+#     'crust': 'thick',
+#     'toppings': ['mushrooms', 'extra cheese']
+# }
+# # Summarize the order
+# print(
+#     f"You ordered a {pizza['crust'].title()} crust pizza with these following toppings:")
+# for topping in pizza['toppings']:
+#     print("\t"+topping)
+
+# favourite_laguages = {
+#     'jen': ['python', 'ruby'],
+#     'sarah': ['c', 'java'],
+#     'edward': ['ruby'],
+#     'phil': ['python', 'r']
+# }
+# for names, languages in favourite_laguages.items():
+#     print(f"{names.title()}'s favourite laguages are:")
+#     for language in languages:
+#         print(f"\t {language.title()}")
+
+# # A DICITONARY IN DICTIONARY
+users = {
+    'aeinstein': {
+        'fname': 'albert',
+        'lname': 'einstein',
+        'location': 'princeton'
+    },
+    'mcurie': {
+        'fname': 'marie',
+        'lname': 'curie',
+        'location': 'paris'
+    }
+}
+for username, userInfo in users.items():
+    print(f"Username: {username}")
+    fullName = f"{userInfo['fname']} {userInfo['lname']}"
+    location = userInfo['location']
+
+    print(f"\tFull name: {fullName.title()}")
+    print(f"\tLocation: {location.title()}")
