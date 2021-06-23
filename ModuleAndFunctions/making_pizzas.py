312cf3062aea3db2120977968af1e23320a00704
hoangphung21 <phunghoang211195@gmail.com>
2021-06-22 22:35:12 -0400

    modules and functions

---
+++ ModuleAndFunctions/making_pizzas.py
@@ -0,0 +1,3 @@
+from pizza import makePizza as mp
+mp('XL', 'pepperoni', 'grilled chicken', 'shrooms')
+mp('MEDIUM', 'ground beef', 'ground chicken')
--- ModuleAndFunctions/pizza.py
+++ ModuleAndFunctions/pizza.py
@@ -3,7 +3,3 @@ def makePizza(size, *toppings):
     print(f"\nMaking a {size} pizza with the following toppings:")
     for topping in toppings:
         print(f" - {topping}")
-
-
-makePizza('L', 'green onions',)
-makePizza('XL', 'shooms', 'cheese', 'grilled chicken')
