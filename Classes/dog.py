77b672083911960aa898d9db0e24d173e7b6c1e3
hoangphung21 <phunghoang211195@gmail.com>
2021-06-28 19:25:20 -0400

    classes

---
+++ Classes/dog.py
@@ -0,0 +1,16 @@
+class Dog:
+    def __init__(self, name, age):
+        """Initialize name and age attribute"""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate a dog sitting in response to a command"""
+        print(f"{self.name} is now sitting")
+
+    def roll_over(self):
+        print(f"{self.name} rolled over")
+
+
+my_dog = Dog("Willy", 4)
+print(f"My dog's name is {my_dog.name}")
